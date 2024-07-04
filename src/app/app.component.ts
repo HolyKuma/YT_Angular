d7263ff4eec87407faa44943854c621d2091787b
HolyKuma <71005749+HolyKuma@users.noreply.github.com>
2024-07-04 18:56:43 +0200

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+  ngOnInit(){
+      this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+}
---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs'
+import { Employee } from './employee';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployeeService{
+    private apiServerUrl = 'http://localhost:8080';
+
+
+    constructor(private http: HttpClient) { }
+
+    public getEmployees(): Observable<Employee[]> {
+        return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+    }
+
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+    }
+
+    public deleteEmployee(employeeId: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+    }
+
+}
