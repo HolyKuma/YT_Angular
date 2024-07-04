8abb5464b618400827645369d5d567bbd27b6108
HolyKuma <71005749+HolyKuma@users.noreply.github.com>
2024-07-04 18:51:17 +0200

    Add files via upload

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/service/EmployeeService.java
@@ -0,0 +1,45 @@
+package tech.getarrays.employeemanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.getarrays.employeemanager.exception.UserNotFoundException;
+import tech.getarrays.employeemanager.model.Employee;
+import tech.getarrays.employeemanager.repo.EmployeeRepo;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+
+    }
+
+    public List<Employee> findAllEmployee() {
+        return employeeRepo.findAll();
+    }
+    public Employee updateEmployee(Employee employee){
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " not found"));
+
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+}
